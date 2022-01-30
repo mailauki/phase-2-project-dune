af83a4f8a4c48072bc630886ba64b3ea228e606d
Julie Evans <juliemevans17@gmail.com>
2022-01-29 23:54:39 -0600

    Add author filter

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -4,7 +4,8 @@ import BookCard from "./BookCard";
 function BookList() {
   const [books, setBooks] = useState([])
   const [updatedBooks, setUpdatedBooks] = useState(books)
-  const [filter, setFilter] = useState("")
+  const [seriesFilter, setSeriesFilter] = useState("")
+  const [authorFilter, setAuthorFilter] = useState("")
 
   useEffect(() => {
     fetch("https://the-dune-api.herokuapp.com/books/22")
@@ -29,23 +30,51 @@ function BookList() {
     })
   }, [books])
 
-  const filteredBooks = updatedBooks.filter(book => {
-    const prettyFilter = filter.split("-").join(" ")
-    if(book.series.toLowerCase().includes(prettyFilter)) return book
+  const filteredBooks = updatedBooks
+  .filter(book => {
+    const prettySeriesFilter = seriesFilter.split("-").join(" ")
+    if(book.series.toLowerCase().includes(prettySeriesFilter)) return book
+  })
+  .filter(book => {
+    const prettyAuthorFilter = authorFilter.split("-").join(" ")
+    if(book.author.length === 2) {
+      if(book.author[0].toLowerCase().includes(prettyAuthorFilter)) {
+        return book
+      }
+      else if(book.author[1].toLowerCase().includes(prettyAuthorFilter)) 
+      {
+        return book
+      }
+    }
+    else {
+      if(book.author.toLowerCase().includes(prettyAuthorFilter)) {
+        return book
+      }
+    }
   })
 
   return (
     <div className="books-container">
-      <label>Filter by Series: </label>
-      <select name="filter" id="filter" onChange={(e) => setFilter(e.target.value)}>
-        <option value="">All</option>
-        <option value="dune-chronicles">Dune Chronicles</option>
-        <option value="legends-of-dune">Legends of Dune</option>
-        <option value="prelude-to-dune">Prelude of Dune</option>
-        <option value="heroes-of-dune">Heroes of Dune</option>
-        <option value="schools-of-dune">Schools of Dune</option>
-      </select>
-
+      <div className="series-filter">
+        <label>Filter by Series: </label>
+        <select name="filter" id="series-filter" onChange={(e) => setSeriesFilter(e.target.value)}>
+          <option value="">All</option>
+          <option value="dune-chronicles">Dune Chronicles</option>
+          <option value="legends-of-dune">Legends of Dune</option>
+          <option value="prelude-to-dune">Prelude of Dune</option>
+          <option value="heroes-of-dune">Heroes of Dune</option>
+          <option value="schools-of-dune">Schools of Dune</option>
+        </select>
+      </div>
+      <div className="author-filter">
+        <label>Filter by Author: </label>
+        <select name="filter" id="author-filter" onChange={(e) => setAuthorFilter(e.target.value)}>
+          <option value="">All</option>
+          <option value="frank-herbert">Frank Herbert</option>
+          <option value="brian-herbert">Brian Herbert</option>
+          <option value="kevin-j.-anderson">Kevin J. Anderson</option>
+        </select>
+      </div>
       <ul className="book-list">
         {filteredBooks.sort((a, b) => a.id - b.id).map(book => 
           <BookCard key={book.id} book={book} />
